5f0c6fb98dd767e6a8bc8e3bc5d168051b3892b8
Mario Lopriore <mariojames211@gmail.com>
2018-10-26 14:16:30 -0700

    Minor tweaks to Facebook Open Graph markup.

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
   <meta name="author" content="Wedgehead">
   <meta property="og:url" content="https://wedgeheadpdx.com">
   <meta property="og:site_name" content="Wedgehead">
-  <meta property="og:title" content="3728 NE Sandy Blvd. PDX OR 97232 12pm-12am, 7 Days a Week">
-  <meta property="og:description" content="503-477-7637 KILLER FOOD, CHEAP BOOZE, PINBALL NIRVANA">
+  <meta property="og:title" content="3728 NE Sandy Blvd. PDX OR 97232 12pm-12am, 7 Days a Week (503) 477-7637">
+  <meta property="og:description" content="KILLER FOOD, CHEAP BOOZE, PINBALL NIRVANA">
   <meta property="og:type" content="website">
   <meta property="og:image" content="https://www.anchortagdesign.com/wedgie/og-markup.jpg">
   <meta property="og:locale" content="en_US">
   <meta name="twitter:card" content="summary">
   <meta name="twitter:title" content="Wedgehead">
-  <meta name="twitter:description" content="3728 NE Sandy Blvd. PDX OR 97232 12pm-12am, 7 Days a Week">
+  <meta name="twitter:description" content="3728 NE Sandy Blvd. PDX OR 97232 12pm-12am, 7 Days a Week (503) 477-7637">
   <meta name="twitter:image" content="https://www.anchortagdesign.com/wedgie/images/og-markup.jpeg">
   <meta name="twitter:image:alt" content="Wedgehead">
   <!-- Stylesheets in the css folder -->
