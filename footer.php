32cbe568dfb4cde3b123a5eae9ba2f6a3b193416
Mario Lopriore <mariojames211@gmail.com>
2018-10-30 09:55:59 -0700

    Favicons, schema markup, footer link images - to be styled.

--- footer.php
+++ footer.php
@@ -3,6 +3,8 @@
     <div class="sub-credit">
       <img src="images/wedgehead-trans-white.png" alt="wedgehead logo">
       <p>&copy; 2018 Wedgehead PDX</p>
+      <a href="https://www.facebook.com/WedgeHeadPDX/" target="_blank" rel="nofollow noopener"><img src="images/facebook-icon.svg" alt="facebook link" title="facebook link" class="social-icon"></a>
+      <a href="https://www.instagram.com/wedgeheadpdx/" target="_blank" rel="nofollow noopener"><img src="images/insta-icon.svg" alt="instagram link" title="instagram link" class="social-icon"></a>      
     </div>
     <div class="sub-credit">
       <img src="images/aa.png" alt="Aaron Lee Photography">
--- index.php
+++ index.php
@@ -41,6 +41,62 @@
     gtag('config', 'UA-97175852-3');
   </script>
 
+  <!-- Schema markup -->
+  <script type='application/ld+json'>
+  {
+    "@context": "http://schema.org/",
+    "@type": "LocalBusiness",
+    "openingHours": "Mo-Su 12:00-24:00",
+    "paymentAccepted": [
+      "cash",
+      "visa",
+      "master card"
+    ],
+    "priceRange": "low",
+    "address": {
+      "@type": "PostalAddress",
+      "addressCountry": "United States",
+      "addressLocality": "Portland",
+      "addressRegion": "Oregon",
+      "postalCode": "97232",
+      "streetAddress": "3728 NE Sandy Boulevard",
+      "email": "alanwedgehead@gmail.com",
+      "telephone": "(503) 477-7637",
+      "description": "Wedgehead PDX: pinball arcade, restaurant & bar",
+      "disambiguatingDescription": "Wedgehead PDX is Portland's premier pinball arcade. Classic and modern pinball machines, full bar and food menu",
+      "mainEntityOfPage": "https://wedgeheadpdx.com/",
+      "name": "Wedgehead PDX"
+    },
+    "email": [
+      "rhodeswedgehead@gmail.com",
+      "alanwedgehead@gmail.com"
+    ],
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingCount": "7",
+      "reviewCount": "7",
+      "bestRating": "5",
+      "ratingValue": "4.7",
+      "worstRating": "4"
+    },
+    "telephone": "(503) 477-7637",
+    "smokingAllowed": "False",
+    "description": "Portland pinball arcade, restaurant, bar",
+    "disambiguatingDescription": "Wedgehead PDX | Portland's premier pinball arcade featuring a full bar and restaurant",
+    "name": "Wedgehead PDX",
+    "image": "https://wedgeheadpdx.com/images/outside.jpg"
+  }
+  </script>
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
 </head>
 
 <body>
