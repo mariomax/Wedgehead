6890153650e8eb7d44d33bf7ff71e09b784a0af9
Mario Lopriore <mariojames211@gmail.com>
2018-11-02 16:18:32 -0700

    Minor updates...

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php header('Cache-Control: max-age=84600'); ?>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="author" content="Wedgehead">
   <meta name="description" content="Wedgehead PDX | Pinball arcade, bar and restaurant | 3728 NE Sandy Blvd. PDX OR 97232 | Open 12pm-12am, 7 Days a Week | Phone (503) 477-7637">
   <meta name="keywords" content="Pinball, Pinball Machine, Portland Pinball, Wedgehead Bar & Restaurant, Hollywood NE Portland, Classic Pinball, Pinball Tournament">
