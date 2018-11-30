05a9f0300976610731d0c0e52d5efdf79333b717
Mario Lopriore <mariojames211@gmail.com>
2018-11-30 14:41:13 -0800

    New hours and menu items.

--- food-menu.php
+++ food-menu.php
@@ -2,8 +2,8 @@
   <h3>The Food Menu:</h3>
   <h4>Bring your appetite!</h4>
   <div class="menu">
-    <img src="images/menu-top.jpg" alt="Wedgehead food menu">
-    <img src="images/menu-bottom.jpg" alt="Wedgehead food menu">
+    <img src="images/new-menu-A.jpg" alt="Wedgehead food menu">
+    <img src="images/new-menu-B.jpg" alt="Wedgehead food menu">
   </div>
   <p>Join us for all Blazer games with food and drink specials.</p>
   <p>Menu specials will change weekly. Please check back regularly for a peek at some of our tasty new creations.</p>
--- header.php
+++ header.php
@@ -5,6 +5,6 @@
 <section class="intro">
   <h1>Wedgehead PDX</h1>
   <h2>Portland's Premier Pinball Arcade!</h2>
-  <h3>3728 NE Sandy Blvd.<br />Portland, OR 97232<br />12pm - 12am | 7 Days a Week</h3>
+  <h3>3728 NE Sandy Blvd.<br />Portland, OR 97232<br />12pm - 12am | Sun - Thurs<br />12pm - 1am | Fri &amp; Sat<br />21 &amp; over after 6:00pm</h3>
   <a href="tel:+1-503-477-7637" class="phone">(503) 477-7637</a>
 </section>
