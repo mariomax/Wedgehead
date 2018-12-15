8c9a5b325bc3bcbe1c1a888f2243b2c0ab6c051f
Mario Lopriore <mariojames211@gmail.com>
2018-12-14 16:06:43 -0800

    Added email link in "About" and contact for events on top.

--- about.php
+++ about.php
@@ -2,7 +2,6 @@
   <div class="about-title">
     <img src="images/wedge-head-logo.png" alt="wedgehead logo" class="wedge-logo">
     <h3>The premier pinball arcade in Portland. Serving bad-ass bar food, stiff drinks and cold beer. Home to 25 rotating pins from all eras.</h3>
-
   </div>
   <figure class="owners">
     <img src="images/rhodes-alan.jpg" alt="Chris Rhodes and Alan Robertson of Wedgehead">
@@ -10,5 +9,26 @@
       Pinball Fiend.</figcaption>
   </figure>
   <p class="p1">Chris Rhodes is the self-proclaimed silverball sultan of wedgehead, bringing in a few dozen of his favorite games to light up the unique gameroom. With well over a hundred games in his personal collection, Rhodes will be curating the best from the 1960s-today and keeping them playing slick and fast. He has been an operator in Portland for over a decade and an avid tournament player/organizer for at least two decades. He's the pinball pusher-man. Just try to leave without a new and worsening habit.</p>
-  <p class="p2">Alan Robertson is the mastermind of the food program at Wedgehead. He's a 16-year industry professional, with experience in all cuisines from fine dining to fast casual. His approach is to deliver inspired bar food, all scratch made in house. Food that you’ll find yourself craving. No trends here. Just the common - overly engineered to be uncommonly great. His previous stints in portland included Clyde Common and as the chef of Bunk Sandwiches.</p>
+  <p class="p2">Alan Robertson is the mastermind of the food program at Wedgehead. He's a 16-year industry professional, with experience in all cuisines from fine dining to fast casual. His approach is to deliver inspired bar food, all scratch made in house. Food that you’ll find yourself craving. No trends here. Just the common - overly engineered to be uncommonly great. His previous stints in portland included Clyde Common and as the chef of Bunk Sandwiches. Contact: <span id="contact"><script type="text/javascript">
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",l=x.length;for(i=0;i<l;i+=2) {if(i+1<l)o+=" +
+"x.charAt(i+1);try{o+=x.charAt(i);}catch(e){}}return o;}f(\"ufcnitnof x({)av" +
+" r,i=o\\\"\\\"o,=l.xelgnhtl,o=;lhwli(e.xhcraoCedtAl(1/)3=!84{)rt{y+xx=l;=+;" +
+"lc}tahce({)}}of(r=i-l;1>i0=i;--{)+ox=c.ahAr(t)i};erutnro s.buts(r,0lo;)f}\\" +
+"\"(0)10\\\\,6\\\"00\\\\\\\\14\\\\00\\\\00\\\\\\\\23\\\\03\\\\03\\\\\\\\06\\" +
+"\\0V\\\\31\\\\0t\\\\\\\\\\\\\\\\Y\\\\\\\\Q\\\\37\\\\0\\\\\\\\\\\\\\\\F\\\\%" +
+"O6 00\\\\\\\\%!*&'$[[RJVZ6X00\\\\\\\\25\\\\0j\\\\U[s_21\\\\0D\\\\OLBYiD13\\" +
+"\\0t\\\\32\\\\0C\\\\PIVJ01\\\\02\\\\00\\\\\\\\sC7r17\\\\\\\\v5ypqzpUwruyjh{" +
+"hkeiei=hqcj\\\\l\\\\\\\"\\\\\\\\\\\\\\\"@\\\\32\\\\06\\\\03\\\\\\\\10\\\\01" +
+"\\\\02\\\\\\\\6X02\\\\\\\\WJ\\\\\\\\\\\\\\\\35\\\\06\\\\03\\\\\\\\24\\\\04\\"+
+"\\00\\\\\\\\06\\\\04\\\\03\\\\\\\\32\\\\0B\\\\37\\\\04\\\\00\\\\\\\\14\\\\0" +
+"5\\\\00\\\\\\\\22\\\\05\\\\00\\\\\\\\\\\\n0\\\\00\\\\\\\\\\\"\\\\f(;} orntu" +
+"re;}))++(y)^(iAtdeCoarchx.e(odrChamCro.fngriSt+=;o27=1y%i;+=)y00=1i=f({i+)i" +
+"+l;i<0;i=r(foh;gten.l=x,l\\\"\\\\\\\"\\\\o=i,r va){,y(x fontincfu)\\\"\")"   ;
+while(x=eval(x));
+//-->
+//]]>
+</script>
+</span></p>
 </section>
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <section class="intro">
   <h1>Wedgehead PDX</h1>
   <h2>Portland's Premier Pinball Arcade!</h2>
-  <h3>3728 NE Sandy Blvd.<br />Portland, OR 97232<br />12pm - 12am | Sun - Thurs<br />12pm - 1am | Fri &amp; Sat<br />21 &amp; over after 6:00pm</h3>
+  <h3>3728 NE Sandy Blvd.<br />Portland, OR 97232<br />12pm - 12am | Sun - Thurs<br />12pm - 1am | Fri &amp; Sat<br /></h3><h4>21 &amp; over after 6:00pm</h4>
   <a href="tel:+1-503-477-7637" class="phone">(503) 477-7637</a>
+  <h4>We host private events and parties!<br /><a href="#contact" title="contact">Contact us for info.</a></h4>
 </section>
